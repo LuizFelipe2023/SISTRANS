0a0ddec6b07a8b730bca7d0d469e803b36505b2c
LuizFelipe2023 <luizfelipefroisneves@gmail.com>
2024-08-23 12:48:32 -0400

    ajustes visuais

--- resources/views/agendamentos/index2.blade.php
+++ resources/views/agendamentos/index2.blade.php
@@ -17,62 +17,36 @@
                 </div>
             @endif
 
-            <table class="table table-bordered table-striped">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Data</th>
-                        <th>Hora</th>
-                        <th>Cliente</th>
-                        <th>Tipo de Carteira</th>
-                        <th>Local</th>
-                        <th>Ações</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @forelse ($agendamentos as $agendamento)
+            <div class="table-responsive">
+                <table class="table table-bordered table-striped">
+                    <thead>
                         <tr>
-                            <td>{{ $agendamento->id }}</td>
-                            <td>{{ \Carbon\Carbon::parse($agendamento->dia_agendamento)->format('d/m/Y') }}</td>
-                            <td>{{ \Carbon\Carbon::parse($agendamento->hora_agendamento)->format('H:i') }}</td>
-                            <td>{{ $agendamento->cliente_nome }} - {{ $agendamento->cliente_cpf }}</td>
-                            <td>{{ ucfirst($agendamento->tipo_carteira) }}</td>
-                            <td>{{ $agendamento->local }}</td>
-                            <td>
-
-                                <button class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#deleteModal-{{ $agendamento->id }}">Cancelar</button>
-
-
-                                <div class="modal fade" id="deleteModal-{{ $agendamento->id }}" tabindex="-1" aria-labelledby="deleteModalLabel-{{ $agendamento->id }}" aria-hidden="true">
-                                    <div class="modal-dialog">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h5 class="modal-title" id="deleteModalLabel-{{ $agendamento->id }}">Confirmar Exclusão</h5>
-                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                            </div>
-                                            <div class="modal-body">
-                                                Tem certeza que deseja cancelar o agendamento de <strong>{{ $agendamento->cliente_nome }}</strong>?
-                                            </div>
-                                            <div class="modal-footer">
-                                                <form method="POST" action="{{ route('agendamentos.destroy', $agendamento->id) }}">
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-                                                    <button type="submit" class="btn btn-danger">Confirmar</button>
-                                                </form>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </td>
+                            <th>ID</th>
+                            <th>Data</th>
+                            <th>Hora</th>
+                            <th>Cliente</th>
+                            <th>Tipo de Carteira</th>
+                            <th>Local</th>
                         </tr>
-                    @empty
-                        <tr>
-                            <td colspan="7" class="text-center">Nenhum agendamento encontrado</td>
-                        </tr>
-                    @endforelse
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        @forelse ($agendamentos as $agendamento)
+                            <tr>
+                                <td>{{ $agendamento->id }}</td>
+                                <td>{{ \Carbon\Carbon::parse($agendamento->dia_agendamento)->format('d/m/Y') }}</td>
+                                <td>{{ \Carbon\Carbon::parse($agendamento->hora_agendamento)->format('H:i') }}</td>
+                                <td>{{ $agendamento->cliente_nome }} - {{ $agendamento->cliente_cpf }}</td>
+                                <td>{{ ucfirst($agendamento->tipo_carteira) }}</td>
+                                <td>{{ $agendamento->local }}</td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="6" class="text-center">Nenhum agendamento encontrado</td>
+                            </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </div>
